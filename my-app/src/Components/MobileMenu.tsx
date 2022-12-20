1de7a0f1bdb6666b1c6890001a254c31534e8a0e
Shinichi <miyakawashi@gmail.com>
2022-12-19 23:38:33 -0500

    mobile menu styling

--- my-app/src/Components/MobileMenu.tsx
+++ my-app/src/Components/MobileMenu.tsx
@@ -4,11 +4,16 @@ interface MobileMenuProps {
 
 export const MobileMenu = ({ menu }: MobileMenuProps) => {
   return (
-    <div className={`mobile-menu-container ${menu ? "mobile-menu-slide-in" : "mobile-menu-slide-out"}`}>
-      <ul>
-        <li>About</li>
-        <li>Work</li>
-        <li>Contact</li>
+    <div
+      className={`mobile-menu-container ${
+        menu ? "mobile-menu-slide-in" : "mobile-menu-slide-out"
+      }`}
+    >
+      <h2>SM</h2>
+      <ul className="mobile-menu-links">
+        <li className="mobile-menu-link">About</li>
+        <li className="mobile-menu-link">Work</li>
+        <li className="mobile-menu-link">Contact</li>
       </ul>
     </div>
   );
