80aa434fb4263c8895a993e7aef1cac9f702e72e
Shinichi <miyakawashi@gmail.com>
2022-12-19 20:07:47 -0500

    mobile navigation basic

--- my-app/src/Components/MobileMenu.tsx
+++ my-app/src/Components/MobileMenu.tsx
@@ -1,3 +1,15 @@
-export const MobileMenu = () => {
-  return <>Testing Mobile Menu</>;
+interface MobileMenuProps {
+  menu: boolean;
+}
+
+export const MobileMenu = ({ menu }: MobileMenuProps) => {
+  return (
+    <div className={`mobile-menu-container ${menu ? "mobile-menu-slide-in" : "mobile-menu-slide-out"}`}>
+      <ul>
+        <li>About</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
 };
--- my-app/src/Components/Navbar.tsx
+++ my-app/src/Components/Navbar.tsx
@@ -1,18 +1,22 @@
 import "../App.css";
 import { Hamburger } from "./Hamburger";
+import { MobileMenu } from "./MobileMenu";
 import React, { useState } from "react";
 
 export const Navbar = () => {
   const [menu, setMenu] = useState<boolean>(false);
 
   return (
-    <nav className="navbar">
-      <h2>SM</h2>
-      <ul>
-        <div className="hamburger">
-          <Hamburger menu={menu} setMenu={setMenu} />
-        </div>
-      </ul>
-    </nav>
+    <>
+      <nav className="navbar">
+        <h2>SM</h2>
+        <ul>
+          <div className="hamburger">
+            <Hamburger menu={menu} setMenu={setMenu} />
+          </div>
+        </ul>
+      </nav>
+      <MobileMenu menu={menu} />
+    </>
   );
 };
