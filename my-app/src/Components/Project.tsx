f2066e55d10d2c378f92a81f42ad4f53712dcc5f
Shinichi <miyakawashi@gmail.com>
2023-01-01 20:33:09 -0500

    project button styles

--- my-app/src/Components/Project.tsx
+++ my-app/src/Components/Project.tsx
@@ -12,7 +12,7 @@ export const Project = ({ projectItem }: ProjectProps) => {
   };
   const handleGitHubRedirect = (event: React.MouseEvent<HTMLElement>): void => {
     event.preventDefault();
-    window.location.href = projectItem.github;
+    window.location.href = projectItem.repo;
   };
   const descriptionShort = (description: string): string => {
     let shortDescription = "";
@@ -26,17 +26,27 @@ export const Project = ({ projectItem }: ProjectProps) => {
   };
 
   return (
-    <Card style={{ position: "static", backgroundColor: "#282c34" }}>
+    <Card
+      style={{
+        position: "static",
+        backgroundColor: "#282c34",
+      }}
+    >
       {/* <Card.Header as="h5">Project One</Card.Header> */}
       <Card.Body>
         <Card.Title className="text-color">{projectItem.name}</Card.Title>
-        <Card.Img variant="top" src={projectItem.image} />
+        <Card.Img
+          variant="top"
+          src={projectItem.image}
+          style={{
+            borderColor: "#eec262",
+            border: "0.1vw solid #eec262",
+          }}
+        />
         {projectItem.description.length > 100 ? (
           <Accordion>
             <Accordion.Item eventKey="0">
-              <Accordion.Header
-                className="d-flex justify-content-between"
-              >
+              <Accordion.Header className="d-flex justify-content-between">
                 Description
               </Accordion.Header>
               <Accordion.Body>{projectItem.description}</Accordion.Body>
@@ -45,12 +55,18 @@ export const Project = ({ projectItem }: ProjectProps) => {
         ) : (
           <Card.Text>{projectItem.description}</Card.Text>
         )}
-        <Button variant="primary" onClick={(e) => handleDemoRedirect(e)}>
-          Demo
-        </Button>
-        <Button variant="primary" onClick={(e) => handleGitHubRedirect(e)}>
-          Github
-        </Button>
+        <div className="project-button-container">
+          <Button
+            variant="primary"
+            style={{}}
+            onClick={(e) => handleDemoRedirect(e)}
+          >
+            Demo
+          </Button>
+          <Button variant="primary" onClick={(e) => handleGitHubRedirect(e)}>
+            Github
+          </Button>
+        </div>
       </Card.Body>
     </Card>
   );
